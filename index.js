38d02522a860b8a7e73888bd1b017f8332453b0e
Jobsidney <jobsidney67@gmail.com>
2022-06-01 16:48:38 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b){
+return a+b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function increment(n){
+    n=n+1;
+    return n;
+}
+increment(715);
+
+function decrement(n){
+    n=n-1
+    return n;
+}
+decrement(72);
+
+parseInt("2.2222", 10);
+
+
+function makeInt(n){
+    return parseInt(n,10);
+}
+makeInt("20");
+function preserveDecimal(n){
+
+   return parseFloat(n); 
+}
+preserveDecimal("80.123999");
